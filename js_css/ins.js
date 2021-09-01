ad05dd95a777d877080394e9bdaf93ed9112c13d
liaochenlanruo <liaochenlanruo@webmail.hzau.edu.cn>
2021-09-01 10:16:17 +0800

    Update ins.js

--- js_css/ins.js
+++ js_css/ins.js
@@ -117,8 +117,8 @@
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
           // 需要修改https://raw.githubusercontent.com/你的github名/你的github仓库名/master/min_photos/即可
-          var minSrc = 'https://github.com/liaochenlanruo/img/master/min_photos/' + data.link[i];
-          var src = 'https://github.com/liaochenlanruo/img/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/liaochenlanruo/img/master/min_photos' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/liaochenlanruo/img/master/photos' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
